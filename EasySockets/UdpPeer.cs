26c550ded86fda485942532f3eb21218989b0d86
M. Fatih Marabaoglu <mfatihmar@monolighttech.com>
2017-06-22 19:48:01 +0300

    Implement UdpPeer

--- EasySockets/UdpPeer.cs
+++ EasySockets/UdpPeer.cs
@@ -1,44 +1,121 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace MFatihMAR.EasySockets
 {
     public class UdpPeer
     {
-        public enum Error
-        {
-            
-        }
-
-        public class Config
-        {
-            public Config(ushort port)
-            {
-            }
-        }
-
         public delegate void StartEvent();
-        public delegate void DataEvent();
-        public delegate void StopEvent();
-        public delegate void ErrorEvent();
+        public delegate void DataEvent(IPEndPoint remoteIPEP, byte[] data);
+        public delegate void StopEvent(Exception exception = null);
 
         public event StartEvent OnStart;
         public event DataEvent OnData;
         public event StopEvent OnStop;
-        public event ErrorEvent OnError;
 
-        public bool IsOpen { get; private set; }
-        public Config Configuration { get; private set; }
+        private ValueWrapper<bool> _isOpen = new ValueWrapper<bool>(false);
+        public bool IsOpen => _isOpen.Value;
+        public IPEndPoint LocalIPEP { get; private set; }
+        public int BufferSize { get; private set; }
 
-        public void Start(Config config)
+        private Socket _socket;
+        private Thread _thread;
+
+        public void Start(IPEndPoint localIPEP, int bufferSize = 512)
         {
+            _Cleanup();
+
+            _isOpen = new ValueWrapper<bool>(true);
+
+            LocalIPEP = localIPEP ?? throw new ArgumentNullException(nameof(localIPEP));
+            BufferSize = bufferSize;
+
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            _socket.Bind(localIPEP);
+
+            _thread = new Thread(_Thread);
+            _thread.Start();
+
+            LocalIPEP = (IPEndPoint)_socket.LocalEndPoint;
+
+            OnStart?.Invoke();
         }
 
         public void Send(IPEndPoint ipep, byte[] data)
         {
+            if (!IsOpen)
+            {
+                return;
+            }
+
+            if (ipep == null)
+            {
+                throw new ArgumentNullException(nameof(ipep));
+            }
+
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (data.Length < 1 || data.Length > BufferSize)
+            {
+                throw new ArgumentOutOfRangeException(nameof(data));
+            }
+
+            _socket.SendTo(data, ipep);
         }
 
         public void Stop()
         {
+            _Cleanup();
+            OnStop?.Invoke();
+        }
+
+        private void _Cleanup()
+        {
+            try
+            {
+                _isOpen.Value = false;
+
+                _socket?.Close();
+
+                if (_thread != null && _thread.IsAlive)
+                {
+                    _thread.Abort();
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void _Thread()
+        {
+            try
+            {
+                var isOpen = _isOpen;
+                var buffer = new byte[BufferSize];
+                var remoteIPEP = (EndPoint)new IPEndPoint(IPAddress.Any, 0);
+
+                while (isOpen.Value)
+                {
+                    var recSize = _socket.ReceiveFrom(buffer, 0, BufferSize, SocketFlags.None, ref remoteIPEP);
+                    var packet = new byte[recSize];
+                    Array.Copy(buffer, packet, recSize);
+                    OnData?.Invoke((IPEndPoint)remoteIPEP, packet);
+                }
+            }
+            catch (ThreadAbortException)
+            {
+            }
+            catch (Exception ex)
+            {
+                _Cleanup();
+                OnStop?.Invoke(ex);
+            }
         }
     }
 }

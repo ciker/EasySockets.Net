eb461e357e794e76072d54483c730363b08b2521
M. Fatih Marabaoglu <mfatihmar@monolighttech.com>
2017-06-26 10:15:25 +0300

    Add IPEndPointParser

---
+++ EasySockets/IPEndPointParser.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+namespace MFatihMAR.EasySockets
+{
+    public static class IPEndPointParser
+    {
+        public static IPEndPoint ToIPEP(this string source)
+        {
+            return null;
+        }
+
+        public static bool TryParse(this IPEndPoint source, string ipepStr)
+        {
+            return false;
+        }
+
+        public static IPEndPoint Parse(string ipepStr)
+        {
+            return null;
+        }
+    }
+}
